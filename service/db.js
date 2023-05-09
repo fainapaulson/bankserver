a31a081d2d541a94e29e5938c64019b17798339c
Faina Paulson <fainapvt@gmail.com>
2023-05-09 12:34:36 +0530

    initialcommit

---
+++ service/db.js
@@ -0,0 +1,17 @@
+// import mongoose
+const mongoose=require('mongoose')
+// connection string
+mongoose.connect('mongodb://localhost:27017/bankServer',{useNewUrlParser:true})
+// model
+const User=mongoose.model('User',
+{
+    acno:Number,
+    uname:String,
+    psw:String,
+    balance:Number,
+    transactions:[]
+})
+// export
+module.exports={
+    User
+}
